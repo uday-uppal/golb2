9717fd73770128595d7df56ba083b58e8599a0ef
udayuppal2 <udayuppal2@gmail.com>
2024-06-23 00:26:15 +0530

    sixth

--- legion/settings.py
+++ legion/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--%p@a!3%&2f-(v*1g33q_m(dkccm5x@!+t#ni*r89&%)k%qqz)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
+ALLOWED_HOSTS =  ['*']
 
 
 # Application definition
