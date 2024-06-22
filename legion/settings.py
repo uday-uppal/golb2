0800ccf29058f7614aa22db3ea5da70f1f6d936e
udayuppal2 <udayuppal2@gmail.com>
2024-06-23 00:20:38 +0530

    fifth

--- legion/settings.py
+++ legion/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--%p@a!3%&2f-(v*1g33q_m(dkccm5x@!+t#ni*r89&%)k%qqz)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS =  ['localhost', '127.0.0.1']
 
 
 # Application definition
