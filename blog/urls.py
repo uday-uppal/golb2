13bc46706c2dabc064b85ec38483a819654f3b7f
udayuppal2 <udayuppal2@gmail.com>
2024-06-30 20:45:47 +0530

    fix

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from django.urls import include, path
 from .views import *
 from django.contrib.sitemaps.views import sitemap 
 from blog.sitemap import BlogModelSitemap 
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('',home, name="home"),
@@ -11,7 +13,9 @@ urlpatterns = [
     path('contact',contact_view, name="contact_url"),
     path('search',SearchView.as_view(), name="search"),
     path('sitemap.xml', sitemap, {'sitemaps': {'blogmodel' : BlogModelSitemap}}, 
-     name='django.contrib.sitemaps.views.sitemap') 
+     name='django.contrib.sitemaps.views.sitemap') ,
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('blog/img/favicon.ico')))
+
 
 
 ]
