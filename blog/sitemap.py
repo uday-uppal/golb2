f79fc95963bce41504b13034abc303ea5dd87de6
udayuppal2 <udayuppal2@gmail.com>
2024-06-30 16:29:38 +0530

    fix

--- blog/sitemap.py
+++ blog/sitemap.py
@@ -2,6 +2,7 @@ from django.contrib.sitemaps import Sitemap
 from .models import BlogModel 
   
 class BlogModelSitemap(Sitemap): 
+    protocol = 'https'
     def items(self): 
         return BlogModel.objects.all().order_by('-created_at') 
         
